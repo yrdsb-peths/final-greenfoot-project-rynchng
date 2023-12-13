cdfd33a881dd93d5cbcbe4c19bf9bc82bf6f1cdf
rynchng <150175907+rynchng@users.noreply.github.com>
2023-12-13 13:52:40 -0500

    updated graphics
    updated the sprite to have the correct rotation, added bullet sprite, made sprite rotation follow cursor

--- Person.java
+++ Person.java
@@ -9,6 +9,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Person extends Actor
 {
     GreenfootImage guy = new GreenfootImage("shoota.png");
+    MouseInfo mouse;
     /**
      * Act - do whatever the Person wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -16,17 +17,59 @@ public class Person extends Actor
     public Person()
     {
         setImage(guy);
-        guy.scale(50,75);
+        guy.scale(90,50);
         setRotation(0);
     }
     
     public void act()
     {
+        mouse = Greenfoot.getMouseInfo();
+        rotate();
+        traverse();
         
+        if (Greenfoot.mouseClicked(null))
+        {
+            shoot();
+        }
     }
     
     public void rotate()
     {
+        mouse = Greenfoot.getMouseInfo();
+        if (mouse != null)
+        {
+            turnTowards(mouse.getX(),mouse.getY());
+        }
+    }
+    
+    public void traverse()
+    {
+        if (Greenfoot.isKeyDown("w"))
+        {
+            setLocation(getX(), getY() - 2);
+        }
         
+        if (Greenfoot.isKeyDown("s"))
+        {
+            setLocation(getX(), getY() + 2);
+        }
+        
+        if (Greenfoot.isKeyDown("a"))
+        {
+            setLocation(getX() - 2, getY());
+        }
+        
+        if (Greenfoot.isKeyDown("d"))
+        {
+            setLocation(getX() + 2, getY());
+        }
+    }
+    
+    public void shoot()
+    {
+        MyWorld gameWorld = (MyWorld) getWorld();
+        mouse = Greenfoot.getMouseInfo();
+        gameWorld.addObject(new Boolet(mouse.getX(), mouse.getY()), getX(), getY()); 
+        System.out.println(getRotation());
     }
 }
